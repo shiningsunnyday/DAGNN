b9bb41bf9939c6b40e2da10f891c1d0867d8f89c
shiningsunnyday <msun415@mit.edu>
2025-06-02 16:24:21 -0400

    r script

--- dvae/bayesian_optimization/evaluate_BN.py
+++ dvae/bayesian_optimization/evaluate_BN.py
@@ -17,8 +17,12 @@ class Eval_BN(object):
             output_file = input_file + "_score"
         input_file = os.path.abspath(input_file)
         output_file = os.path.abspath(output_file)
-        cmd = 'bash -i -c "conda activate cktgnn_r && cd ~/induction/dagnn/dvae/bayesian_optimization && Rscript compute_score.R ' + input_file + ' ' + output_file + ' &"'
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        cmd = (
+            'bash -c "source $(conda info --base)/etc/profile.d/conda.sh && conda activate cktgnn_r && cd {} && Rscript compute_score.R {} {}"'
+        ).format(current_dir, input_file, output_file)
         # cmd = 'cd ~/induction/dagnn/dvae/bayesian_optimization && Rscript compute_score.R ' + input_file + ' ' + output_file
         os.system(cmd)
         score = np.loadtxt(output_file, ndmin=1)
+        print(score)
         return float(score[0])
